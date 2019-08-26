91e3334c8561241e2815269ee43cdee340630415
allen <allenlin@disroot.org>
2019-08-26 20:19:42 +0800

    beautify banner

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -3,7 +3,7 @@ import React from "react";
 const Landing = () => {
   return (
     <div className={"hero"}>
-      <div className={"hero-body"}>
+      <div className={"hero-body title"}>
         <div className={"container"}>
           <h1>
             IT'S <br />
