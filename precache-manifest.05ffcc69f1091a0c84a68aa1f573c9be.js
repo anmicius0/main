1583289f485902f9375583e3c25da6eee9899c6a
allen <allenlin@disroot.org>
2019-09-19 13:01:05 +0800

    Updates

---
+++ precache-manifest.05ffcc69f1091a0c84a68aa1f573c9be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f769901dfecfbe3a0eba1b7756920978",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "04fc38deea0bb78f0fe5",
+    "url": "/main/static/css/2.cf291e2b.chunk.css"
+  },
+  {
+    "revision": "6eb4214a3771c7f22054",
+    "url": "/main/static/css/main.31eaa25b.chunk.css"
+  },
+  {
+    "revision": "04fc38deea0bb78f0fe5",
+    "url": "/main/static/js/2.08d07b35.chunk.js"
+  },
+  {
+    "revision": "6eb4214a3771c7f22054",
+    "url": "/main/static/js/main.900a5b6c.chunk.js"
+  },
+  {
+    "revision": "a2623b626eecddf81ea0",
+    "url": "/main/static/js/runtime~main.f27e0267.js"
+  },
+  {
+    "revision": "3c661fb83311a9af3fb2e2ce5baf5371",
+    "url": "/main/static/media/Cantarell-Bold.3c661fb8.ttf"
+  },
+  {
+    "revision": "ea5ec473618e16d5337d72a85cf085cb",
+    "url": "/main/static/media/Cantarell-Regular.ea5ec473.ttf"
+  },
+  {
+    "revision": "05078759185c986d15c17e7ee8deff43",
+    "url": "/main/static/media/DINPro-Bold_13934.05078759.ttf"
+  },
+  {
+    "revision": "a0099b03e6be26a804d988afb39f4ad7",
+    "url": "/main/static/media/logo.a0099b03.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.349e5e59f976e095f947cd6282b5ab3e.js"
+  "/main/precache-manifest.05ffcc69f1091a0c84a68aa1f573c9be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/main/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
