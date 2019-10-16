9c8e446f6dab7f10f473b65e38242c489115097a
allen <allenlin@disroot.org>
2019-10-16 20:54:31 +0800

    reset package.js>scripts

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div id="footer">
-      Made with &nbsp; <i class="far fa-heart"></i> &nbsp; by ANMICIUS.
+      Made with &nbsp; <i class="far fa-heart"></i> &nbsp; by ANMICIUS. With
+      support from: <Link to="https://buttercms.com">ButterCMS</Link>
     </div>
   );
 };
